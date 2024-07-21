7e46e90b47239005678af024d6eca03d440ac96d
easyscripter <deniscs49@gmail.com>
2024-07-21 23:21:52 +0300

    fix: add loading state

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,30 +1,31 @@
-"use client"
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { LoginForm, LoginFormData } from '@/components/auth/loginForm';
 import { useToast } from '@/components/ui/use-toast';
 import { signIn } from 'next-auth/react';
 
 const SignInPage = () => {
-    const {toast} = useToast();
-    const handleLogin =  async (data: LoginFormData) => {
-        const user = await signIn('credentials', {
-            username: data.username,
-            password: data.password,
-            redirect: false
-        });
-        if (user?.error) {
-            toast({
-                title: 'Ошибка',
-                description: user.error,
-                variant: 'destructive',
-            });
-            return;
-        }
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+  const handleLogin = async (data: LoginFormData) => {
+    setIsLoading(true);
+    const user = await signIn('credentials', {
+      username: data.username,
+      password: data.password,
+      redirect: false,
+    });
+    setIsLoading(false);
+    if (user?.error) {
+      toast({
+        title: 'Ошибка',
+        description: user.error,
+        variant: 'destructive',
+      });
+      return;
     }
+  };
 
-    return (
-        <LoginForm onLogin={handleLogin}/>
-    );
-}
- 
+  return <LoginForm isLoading={isLoading} onLogin={handleLogin} />;
+};
+
 export default SignInPage;
--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -1,7 +1,7 @@
-"use client"
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+'use client';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import {
   Form,
   FormControl,
@@ -9,58 +9,72 @@ import {
   FormLabel,
   FormField,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { loginSchema } from '@/validationSchemas'
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { loginSchema } from '@/validationSchemas';
 
 export type LoginFormData = z.infer<typeof loginSchema>;
 
 type LoginFormProps = {
-    onLogin: (data: LoginFormData) => void
-}
+  onLogin: (data: LoginFormData) => void;
+  isLoading?: boolean;
+};
 
-export const LoginForm = ({onLogin}: LoginFormProps) => {
+export const LoginForm = ({ onLogin, isLoading }: LoginFormProps) => {
+  const form = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
 
-    const form = useForm<LoginFormData>({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-            username: "",
-            password: "",
-        }
-    })
+  const onSubmit = (data: LoginFormData) => {
+    onLogin(data);
+  };
 
-    const onSubmit = (data: LoginFormData) => {
-        onLogin(data);
-    }
-
-
-    return (
-        <Form {...form} >
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6 bg-white p-8 border-2 rounded-lg w-[400px] shadow-md'>
-                <h2 className='text-2xl text-center font-bold'>Авторизация</h2>
-                <FormField name="username" control={form.control} render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Имя пользователя:</FormLabel>
-                        <FormControl>
-                            <Input placeholder="Имя пользователя" {...field} />
-                        </FormControl>
-                        {form.formState.errors.username && <FormMessage>{form.formState.errors.username.message}</FormMessage>}
-                    </FormItem>
-                )}
-                />
-                <FormField name="password" control={form.control} render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Пароль:</FormLabel>
-                        <FormControl>
-                            <Input type="password" placeholder="Пароль" {...field} />
-                        </FormControl>
-                         {form.formState.errors.password && <FormMessage>{form.formState.errors.password.message}</FormMessage>}
-                    </FormItem>
-                )}
-                />
-                <Button className='w-full bg-blue-500' type="submit">Вход</Button>
-            </form>
-        </Form>
-    )
-}
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-[400px] space-y-6 rounded-lg border-2 bg-white p-8 shadow-md"
+      >
+        <h2 className="text-center text-2xl font-bold">Авторизация</h2>
+        <FormField
+          name="username"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Имя пользователя:</FormLabel>
+              <FormControl>
+                <Input placeholder="Имя пользователя" {...field} />
+              </FormControl>
+              {form.formState.errors.username && (
+                <FormMessage>{form.formState.errors.username.message}</FormMessage>
+              )}
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="password"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Пароль:</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Пароль" {...field} />
+              </FormControl>
+              {form.formState.errors.password && (
+                <FormMessage>{form.formState.errors.password.message}</FormMessage>
+              )}
+            </FormItem>
+          )}
+        />
+        <Button disabled={isLoading} className="w-full bg-blue-500" type="submit">
+          {isLoading ? 'Загрузка...' : 'Войти'}
+        </Button>
+      </form>
+    </Form>
+  );
+};
