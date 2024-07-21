ccec382da7db9ec2880f8794b133e8a96e630969
easyscripter <deniscs49@gmail.com>
2024-07-21 22:36:56 +0300

    fix: add base rules for prettier and eslint

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { db } from '@/lib/db';
 
 type AuthLayoutProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 const AuthLayout = async ({ children }: AuthLayoutProps) => {
-    return ( <div className='h-full flex items-center justify-center'>{children}</div> );
-}
- 
+  return <div className="flex h-full items-center justify-center">{children}</div>;
+};
+
 export default AuthLayout;
